771ef7d86c72da757cd62176f98436634811a4bd
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-03-12 22:11:43 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import './App.css';
-
-function App() {
+import { BrowserRouter as Router,Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+ function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar title={"Github Finder"}/>
+
+         <main className="container mx-auto px-3 pb-12">Component</main>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Navbar from "./components/layout/Navbar";
+
+// function App() {
+//   return (
+//    <h1 className="text-red-500 text-5xl">TAILWIND TEST</h1>
+//   );
+// }
+
+// export default App;
+
+
+
+// <Router>
+//       <div className="flex flex-col justify-between h-screen">
+//         <Navbar />
+
+//         <main className="container mx-auto px-3 pb-12 flex-grow">
+//           Component
+//         </main>
+//       </div>
+//     </Router>
