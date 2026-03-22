c94c052111a0aae60f37f986158c8fe3127e27f3
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-03-22 22:15:10 +0530

    Added handleSubmit and handleChange

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -2,6 +2,8 @@ const githubReducer=(state,action)=>{
     switch (action.type){
 
         // This "case" means that the return statement will only run if and only if when we dispatch(format for dispatch is:- dispatch({type:"GET_USERS", payload:data,}))
+        // the payload is used to hold the current data
+        // The reducer function receives the current state and the action object. It uses the action.type to determine the operation and accesses action.payload to get the necessary data.
         case "GET_USERS":
             return{
                 ...state,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
 function Home() {
   console.log(process.env);
   return (
     <>
+      <UserSearch />
       <UserResults />
     </>
   );
