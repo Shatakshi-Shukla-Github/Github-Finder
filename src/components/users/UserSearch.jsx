2022a81db3cff0328889ed816ad767c885368627
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-03-25 20:57:25 +0530

    Solved User Search bug

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -13,6 +13,7 @@ function UserSearch() {
       setText("");
     }
   };
+
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       <div>
@@ -24,6 +25,7 @@ function UserSearch() {
                 className="w-full pr-40 bg-gray-200 input input-lg text-black"
                 placeholder="Search"
                 onChange={handleChange}
+                value={text}
               />
               <button className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">
                 Go
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,7 +3,7 @@
 import { createContext, useReducer } from "react";
 import githubReducer from "./GithubReducer";
 
-// This creates an empty container
+// This creates an empty container:-
 const GithubContext = createContext();
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
@@ -28,7 +28,7 @@ export const GithubProvider = ({ children }) => {
     const params=new URLSearchParams({
       q:text
     })
-    const response = await fetch(`${GITHUB_URL}/search/users/?${params}`);
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
     const {items} = await response.json();
     dispatch({
       type:"GET_USERS",
@@ -36,14 +36,16 @@ export const GithubProvider = ({ children }) => {
     })
   };
   const setLoading=()=>dispatch({type:"SET_LOADING"})
-
+  // const clearUsers=()=>{
+  //   dispatch({type:"DELETE_USERS"})
+  // }
   return (
     // You are putting inside the box:
     // GithubContext contains:
     // 1) users
     // 2) loading
     // 3) fetchUsers
-    <GithubContext.Provider value={{ users:state.users, loading:state.loading, searchUsers }}>
+    <GithubContext.Provider value={{ users:state.users, loading:state.loading, searchUsers, }}>
       {/* What is {children}?
       This means:
       “Whatever is wrapped inside GithubProvider in App.js” */}
