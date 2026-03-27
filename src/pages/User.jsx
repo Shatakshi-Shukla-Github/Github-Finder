3de1af49f69ba0fd82a566ec031f5b4054ae54bb
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-03-28 00:18:42 +0530

    Added Back to Search Link in User Component

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,6 +1,8 @@
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { useContext, useEffect } from "react";
 import GithubContext from "../context/github/GithubContext";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Spinner from "../components/layout/assets/Spinner";
 function User() {
   const params = useParams();
@@ -8,10 +10,36 @@ function User() {
   useEffect(() => {
     getUser(params.login);
   }, [params.login]);
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
   if (loading) {
     return <Spinner />;
   } else {
-    return <div>{user?.login}</div>;
+    return (
+      <>
+        <div className="w-full mx-auto lg:w-10/12">
+          <div className="mb-4">
+            <Link to="/" className="btn btn-ghost">
+              Back To Search
+            </Link>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 export default User;
