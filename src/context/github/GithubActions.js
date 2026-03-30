9e8ea17701d39e96698dbbc4fa22e50a133974f3
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-03-30 21:20:44 +0530

    Cleaned up the function and Added Axios

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,33 +1,22 @@
+import axios from "axios";
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-
+const github=axios.create({
+  baseURL:GITHUB_URL,
+})
 export const searchUsers = async (text) => {
     const params=new URLSearchParams({
       q:text
     })
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
-    const {items} = await response.json();
-    return items
+    // Axios automatically gives the JSON data so we dont have to convert it to JSON data by doing response.json
+    const response=await github.get(`/search/users?${params}`)
+    return response.data.items
   };
 
-//Get Single User:-
-export const getUser= async (login) => {
-    let response = await fetch(`${GITHUB_URL}/users/${login}`);
-    if(response.status===404){
-      window.location="/notfound"
-    }else{
-     const data = await response.json();
-      return data
-    }
-  };
-
-
-  //Get User Repos:-
-  export const getUserRepos=async (login) => {
-    const params=new URLSearchParams({
-      sort:"created",
-      per_page:10,
-    })
-const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
-    const data = await response.json();
-    return data
-  }  
+//Get User and Repos:-
+export const getUserAndRepos=async(login)=>{
+  const [user,repos]=await Promise.all([
+    (await github.get(`/users/${login}`)),
+    github.get(`users/${login}/repos`),
+  ])
+  return {user:user.data,repos:repos.data}
+}
