7f2954502da5170deeb9288613cbe615ab975ed4
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-03-30 22:02:25 +0530

    Removed PropTypes

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,5 @@
 import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
 function Navbar({ title }) {
   return (
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
